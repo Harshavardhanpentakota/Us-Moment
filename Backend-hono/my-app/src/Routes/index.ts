9c68d2a0e462a354ef2a97f887fc254b790484f5
Harsha Vardhan Pentakota <harshavardhan.pentakota19@gmail.com>
2024-09-01 16:28:19 +0530

    Deleted unnecessary

--- Backend-hono/my-app/src/Routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from 'hono';
-import { accountRouter } from './account';
-import { roadMapRouter } from './roadmap';
-
-const mainRouter = new Hono();
-mainRouter.route('/account', accountRouter);
-mainRouter.route('/roadmaps', roadMapRouter);
-
-export { mainRouter };

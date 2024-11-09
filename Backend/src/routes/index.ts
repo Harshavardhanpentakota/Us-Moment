d5daba1b81c9ac1b7d1965152106c815e6de824d
Harsha Vardhan Pentakota <harshavardhan.pentakota19@gmail.com>
2024-11-09 12:20:10 +0530

    Backend temp removed

--- Backend/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import {Route, Router} from "react-router-dom"
-import express from "express"
-import { accountRouter } from "./account";
-
-const mainRouter = express.Router();
-mainRouter.use("/account", accountRouter);
-
-export {mainRouter};

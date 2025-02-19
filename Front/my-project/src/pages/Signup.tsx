d02005549c36e89d1cfc5211de22dd7e5798dac3
Harsha Vardhan Pentakota <harshavardhan.pentakota19@gmail.com>
2025-02-19 07:10:17 +0530

    Signin,Signup Pages fixed

--- Front/my-project/src/pages/Signup.tsx
+++ Front/my-project/src/pages/Signup.tsx
@@ -7,15 +7,14 @@ const Signup = () => {
           <div className="flex items-center text-3xl font-black">UsMoment</div>
           <blockquote className="space-y-2">
             <p className="text-lg font-medium">
-              "This library has saved me countless hours of work and helped me
-              deliver stunning designs to my clients faster than ever before."
+              With great power comes great responsibility
             </p>
-            <footer className="text-sm">Sofia Davis</footer>
+            <footer className="text-sm">Uncle Ben</footer>
           </blockquote>
         </div>
         <div className="col-span-2 bg-white flex flex-col justify-center items-center p-8 lg:col-span-1">
           <div className="flex justify-center items-center">
-            <SignUp forceRedirectUrl={"/"} />
+            <SignUp/>
           </div>
         </div>
       </div>
